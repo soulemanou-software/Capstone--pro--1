5fe61f30cf643ad67ce98d96498d28558f2235a9
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-06-03 16:05:49 +0100

    mobile menu function 100%

--- js/app.js
+++ js/app.js
@@ -13,57 +13,57 @@ mainNav.addEventListener('click', (event) => {
   }
 });
 
-/*here is for the lecturas data cart creation */
+/* here is for the lecturas data cart creation */
 
 const features = [
-    {
-      id: 1,
-      photo: './Resources/images/11 PM.png',
-      name: 'Soulemanou Babanou',
-      role: 'C.E.O & Founder Of Fastdevz',
-      topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
-    },
-    {
-      id: 2,
-      photo: './Resources/images/speaker_01.png',
-      name: 'John Who ',
-      role: 'Sotware Engineer and teacher of DBMS',
-      topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
-    },
-    {
-      id: 3,
-      photo: './Resources/images/speaker_03.png',
-      name: 'Peter Code',
-      role: 'Back-end developer and mentor at Fastdevz',
-      topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
-    },
-    {
-      id: 4,
-      photo: './Resources/images/speaker_04.png',
-      name: 'Rames Pires',
-      role: 'Co-founder of Fastdevz',
-      topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
-    },
-    {
-      id: 5,
-      photo: './Resources/images/speaker_05.png',
-      name: 'Astrid Mugisha',
-      role: 'Secretary and legal laws professor',
-      topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
-    },
-    {
-      id: 6,
-      photo: './Resources/images/speaker_06.png',
-      name: 'Claudia',
-      role: 'H.O.D of social science with master degree',
-      topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
-    },
-  ];
-  
-  for (let n = 0; n < features.length; n += 1) {
-    const feature = features[n];
-  
-    featureSection.innerHTML += `
+  {
+    id: 1,
+    photo: './Resources/images/11 PM.png',
+    name: 'Soulemanou Babanou',
+    role: 'C.E.O & Founder Of Fastdevz',
+    topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
+  },
+  {
+    id: 2,
+    photo: './Resources/images/speaker_01.png',
+    name: 'John Who ',
+    role: 'Sotware Engineer and teacher of DBMS',
+    topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
+  },
+  {
+    id: 3,
+    photo: './Resources/images/speaker_03.png',
+    name: 'Peter Code',
+    role: 'Back-end developer and mentor at Fastdevz',
+    topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
+  },
+  {
+    id: 4,
+    photo: './Resources/images/speaker_04.png',
+    name: 'Rames Pires',
+    role: 'Co-founder of Fastdevz',
+    topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
+  },
+  {
+    id: 5,
+    photo: './Resources/images/speaker_05.png',
+    name: 'Astrid Mugisha',
+    role: 'Secretary and legal laws professor',
+    topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
+  },
+  {
+    id: 6,
+    photo: './Resources/images/speaker_06.png',
+    name: 'Claudia',
+    role: 'H.O.D of social science with master degree',
+    topic: 'A computer would deserve to be called intelligent if it could deceive a human into believing that it was human',
+  },
+];
+
+for (let n = 0; n < features.length; n += 1) {
+  const feature = features[n];
+
+  featureSection.innerHTML += `
     <div class="featured-cart feature-cart-content" >
         <div class="teach-img">
             <img src="${feature.photo}" alt="teacher_pic" class="teacher_pic">
@@ -76,4 +76,4 @@ const features = [
             </div>
          </div>
     `;
-  }
+}
