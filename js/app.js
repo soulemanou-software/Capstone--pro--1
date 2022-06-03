5fcc959c28fd0828717352d22f88ead2d440a58b
soulemanou kunchiefuh babanou <57832090+soulemanou-software@users.noreply.github.com>
2022-06-03 12:29:55 +0100

    mobile menu function done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-const a = 'good';
+// Mobile menu function//
+const menuBtn = document.getElementById('menuBtn');
+const mainNav = document.getElementById('mainNav');
+
+menuBtn.addEventListener('click', () => {
+  mainNav.classList.add('show');
+});
+
+mainNav.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A' || event.target.tagName === 'I') {
+    mainNav.classList.remove('show');
+  }
+});
